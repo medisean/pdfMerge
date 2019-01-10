1ca542275da4a86da1b820b0a5106c313fa09b33
zouliangming <liangming@teambition.com>
2019-01-10 14:26:16 +0800

    init project

---
+++ pdfMerge/pdfMerge/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pdfMerge
+//
+//  Created by bruce on 2019/1/10.
+//  Copyright © 2019 ZLM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pdfMerge/pdfMerge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pdfMerge
+//
+//  Created by bruce on 2019/1/10.
+//  Copyright © 2019 ZLM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
