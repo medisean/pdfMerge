510f0e10bdafe61cb9dc5d5f6d3f4e76f5c0b36e
zouliangming <liangming@teambition.com>
2019-01-10 15:48:59 +0800

    support merge pdf

--- pdfMerge/pdfMerge/ViewController.swift
+++ pdfMerge/pdfMerge/ViewController.swift
@@ -10,10 +10,53 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var file1: URL?
+    var file2: URL?
+    var final: URL?
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "pdf 合并"
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func file1ButtonPressed(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.prompt = "打开"
+        openPanel.allowedFileTypes = ["pdf"]
+        openPanel.directoryURL = nil
+        openPanel.beginSheetModal(for: self.view.window!) { (returnCode) in
+            if returnCode.rawValue == 1 { // success
+                guard let fileURL = openPanel.urls.first else {
+                    return
+                }
+                self.file1 = fileURL
+            }
+        }
+    }
+    
+    @IBAction func file2ButtonPressed(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.prompt = "打开"
+        openPanel.allowedFileTypes = ["pdf"]
+        openPanel.directoryURL = nil
+        openPanel.beginSheetModal(for: self.view.window!) { (returnCode) in
+            if returnCode.rawValue == 1 { // success
+                guard let fileURL = openPanel.urls.first else {
+                    return
+                }
+                self.file2 = fileURL
+            }
+        }
+    }
+    
+    @IBAction func mergeButtonPressed(_ sender: Any) {
+        guard let file1URL = file1, let file2URL = file2 else {
+            return
+        }
+
+        PdfMergeUtil.init().mergeTwoFiles(file1URL: file1URL, file2URL: file2URL)
     }
 
     override var representedObject: Any? {
@@ -21,7 +64,5 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
